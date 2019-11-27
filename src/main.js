9af4c2b51be02c1ed79038c1d4a418540c7f4b3d
wanghui <1027332684@qq.com>
2019-11-27 16:51:53 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {HashRouter} from 'react-router-dom'
-import {Provider} from 'react-redux';
+import { render } from 'react-dom';
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './store'
+import store from './Redux'
 
 render(
     <Provider store={store}>
         <HashRouter>
-            {/* <Route component={App}/> */}
-            <App/>
+            <App />
         </HashRouter>
     </Provider>,
     document.querySelector('#app')
