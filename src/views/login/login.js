e64176479e8ec99f9a2b4e952a70c1197d319ea8
zsk <1959153881@qq.com>
2019-11-29 21:20:22 +0800

    坤哥修改

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,56 +1,101 @@
 import React,{Component} from 'react';
-import { Form, Icon, Input, Button, Checkbox,Divider  } from 'antd';
+import { Form, Icon, Input, Button, Checkbox,Divider,label  } from 'antd';
 import '../../common/css/mine/login.scss';
-
+import { my } from '../../api';
+import {Redirect} from 'react-router-dom'
+// import {connect} from 'react-redux'
+// @connect(
+//     state => state.phone,
+//     {login}
+//   )
 class Login extends Component{
+  
+
   handleSubmit = e => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    this.props.form.validateFields(async(err, values) => {
+      let { phone, password } = values
       if (!err) {
-        console.log('Received values of form: ', values);
-      }
+        let {
+            data
+        } = await my.get("/login", {
+            phone,
+            password
+        });
+        console.log(data);
+
+        if (data.status == 1) {
+            this.props.history.replace('/')
+        } 
+
+
+    }
     });
   };
+  validatorPwd = (rule, value, callback) => {
+    if (!value) {
+        
+    }
+  }
+  constructor(props){
+    super(props);
+    this.register = this.register.bind(this);
+}
+  register(){
+    console.log(this.props);
+    this.props.history.push('/reg')
+  }
+  handleLogin(){ 
+
+    this.props.login(this.props.state)
+    // console.log("this.props.state",this.props.state);
+}
   render() {
 
     const { getFieldDecorator } = this.props.form;
     return (
-    
+      
       <Form onSubmit={this.handleSubmit} className="login-form">
-        <div className="login_header">登录8只小猪</div>
+        {this.props.redirectTo&&this.props.redirectTo!=='/login'? <Redirect to={this.props.redirectTo} />:null}
+        <div className="logheader">
+        <Button type="link" className="goback" onClick={()=>this.goBack()}>
+        <Icon type="left" />
+        </Button><p className="login_header">登录8只小猪</p>
+        </div>
+        <br/>
         <Form.Item>
+          <label>账号：</label>
           {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+            // rules: [{ required: true, message: 'Please input your phone!' }],
           })(
+            
             <Input
               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
+              placeholder="请输入手机号"
             />,
           )}
         </Form.Item>
         <Form.Item>
+          <label>密码：</label>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            // rules: [{ required: true, message: 'Please input your Password!' }],
           })(
+            
             <Input
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
               type="password"
-              placeholder="Password"
+              placeholder="请输入密码"
             />,
           )}
         </Form.Item>
         <Form.Item>
-          {/* {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(<Checkbox>Remember me</Checkbox>)} */}
           <a className="reg-form-a" href="">
             短信验证登录
           </a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
+          <Button type="" htmlType="submit" className="login-form-button">
             登录
           </Button>
-          <Button type="primary" htmlType="submit" className="reg-form-button">
+          <Button type="primary" htmlType="submit" className="reg-form-button" onClick={this.register}>
             注册
           </Button>
         </Form.Item>
