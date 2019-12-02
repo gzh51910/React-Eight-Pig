d6b58d82d5f763cb00e18f93e24449947797e747
zdd <291856414@qq.com.com>
2019-12-02 20:06:40 +0800

    2

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
-import cartReducer from './cart';
 import commonReducer from './common';
 
 let rootReducer = combineReducers({
-    cart:cartReducer,
     common:commonReducer
 })
 export default rootReducer;
